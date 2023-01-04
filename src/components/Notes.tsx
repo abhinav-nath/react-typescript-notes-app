dfadc35650793d8398796049cf305051b6d9e5a5
Abhinav Nath <abhinavnath@ymail.com>
2023-01-05 00:42:51 +0530

    Add styling to note card

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,12 +1,26 @@
 import * as React from "react";
+import { Button, Card } from "react-bootstrap";
 import { Note } from "../models/Note";
 
 interface INotesProps {
   note: Note;
 }
 
-const Notes: React.FunctionComponent<INotesProps> = ({ note }) => {
-  return <div>Note Card</div>;
+const Notes: React.FC<INotesProps> = ({ note }) => {
+  return (
+    <div className="mb-3">
+      <Card style={{ backgroundColor: note.color }}>
+        <Card.Body>
+          <Card.Title>{note.title}</Card.Title>
+          <Card.Text>{note.text}</Card.Text>
+          <Card.Subtitle className="text-muted">{note.date}</Card.Subtitle>
+          <Button className="mt-3" variant="danger">
+            Delete
+          </Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Notes;
